e01741591f3edc457311ec08773581483e48a2e0
kejiantong <tong.ke@northeastern.edu>
2022-03-16 16:07:46 -0700

    add restaurant model

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Restaurant.java
@@ -0,0 +1,15 @@
+package edu.northeastern.cs5500.starterbot.model;
+
+import java.util.ArrayList;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+@Data
+public class Restaurant implements Model {
+    ObjectId id;
+    String name;
+    String address;
+    Long contact;
+    ArrayList<String> cuisineType;
+    ArrayList<DishObject> menu;
+}
