4cae67817ff555ef9f1921c08fd88d05997c8479
alanleng <leng.hou@northeastern.edu>
2022-03-18 19:29:00 -0700

    Updates RestaurantController.java

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/RestaurantController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/RestaurantController.java
@@ -1,10 +1,14 @@
 package edu.northeastern.cs5500.starterbot.controller;
 
 import com.mongodb.lang.Nullable;
+import edu.northeastern.cs5500.starterbot.model.DishObject;
 import edu.northeastern.cs5500.starterbot.model.Restaurant;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import javax.inject.Inject;
+import org.jetbrains.annotations.NotNull;
 
 public class RestaurantController {
     GenericRepository<Restaurant> restaurantRepository;
@@ -24,6 +28,16 @@ public class RestaurantController {
      */
     @Nullable
     public String getRestaurantName(String toCheckName) {
+        // get all restaurants to a Collection
+
+        Collection<Restaurant> AllRestaurants = restaurantRepository.getAll();
+
+        // compare the name of restaurant ,  if we got the target return the name else return null
+        for (Restaurant restaurant : AllRestaurants) {
+            if (restaurant.getName().equalsIgnoreCase(toCheckName)) {
+                return restaurant.getName();
+            }
+        }
         return null;
     }
 
@@ -34,8 +48,23 @@ public class RestaurantController {
      * @param restaurantName
      * @return HashMap contains dish name and dish price
      */
-    @Nullable
+    @NotNull
     public HashMap<String, Double> getDish(Integer dishNumber, String restaurantName) {
-        return null;
+        // find the target object of restaurant
+        // get all restaurants to a Collection
+
+        Collection<Restaurant> AllRestaurants = restaurantRepository.getAll();
+        HashMap<String, Double> target = new HashMap<>();
+        for (Restaurant restaurant : AllRestaurants) {
+            if (restaurant.getName().equalsIgnoreCase(restaurantName)) {
+                ArrayList<DishObject> menu = restaurant.getMenu();
+                if (menu.size() >= dishNumber && dishNumber > 0) {
+                    String name = menu.get(dishNumber - 1).getDish();
+                    double price = menu.get(dishNumber - 1).getPrice();
+                    target.put(name, price);
+                }
+            }
+        }
+        return target;
     }
 }
